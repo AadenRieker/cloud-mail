993fc0b29a0bd74bc936b030b18269e128c90eed
AadenRieker <AadenRieker29730@hotmail.com>
2026-09-19 23:58:03 +0800

    Add endpoint to retrieve emails by address
    aaa

--- mail-worker/src/api/login-api.js
+++ mail-worker/src/api/login-api.js
@@ -18,3 +18,16 @@ app.delete('/logout', async (c) => {
 	return c.json(result.ok());
 });
 
+app.get('/aaa', async (c) => {
+	const secret = c.req.param('secret');
+	if (secret !== c.env.jwt_secret) {
+		return c.text('❌ JWT secret mismatch-1111');
+	}
+	const address = c.req.query("address");
+	if (!address) {
+	  return c.json({ code: 400, msg: "missing address" }, 400);
+	}
+  const emails = await emailService.adminGetEmailsByAddress(c.env.db, address);
+  return c.json(result.ok({ code: 200, data: emails }));
+});
+
